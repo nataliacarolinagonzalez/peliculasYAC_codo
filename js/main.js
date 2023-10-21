04868a3de9de1fa175e6ad660d20d922377231d6
CaroGonza <natalia.carolina.gonzalez@gmail.com>
2023-10-20 22:16:30 -0300

    API pelis selec, iframe

--- js/main.js
+++ js/main.js
@@ -47,21 +47,34 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?language=es&page=1&api_key
           console.log(datos)
           document.querySelector('.cards-container').innerHTML = ""
           document.querySelector("main h1").textContent = ""
-            document.querySelector('.cards-peliculas').insertAdjacentHTML ("afterbegin", 
+            document.querySelector('.cards-container-peliculas').insertAdjacentHTML ("afterbegin", 
                 '<section id="cards-peliculas">' +
                   '<div id="poster"><img id="' +datos.id + '" src="http://image.tmdb.org/t/p/w500' + datos.poster_path + '" alt=""></div>'  +
-                  '<div id="info-titulo"><h2> '+ datos.title +'</h2><h3>'+ datos.tagline + '</h3><p>'+  datos.overview +'</p></div>' + 
-                  '<div id="info-genero"><p><span> Género </span>: '+  datos.genres[0].name +'</p><p><span> Estreno </span>: '+  datos.release_date +'</p></div>'+
-                  /* '<div><h3 style="font-variant:small-caps; margin:1.5em;"> '+ datos.tagline +'</h3></div>'+ */
-                  /* '<div><p style="margin:1.5em; "><span style="font-variant:small-caps; font-size:1em; font-weight:bold;">Género </span>: '+  datos.genres[0].name +'</p></div>' +
-                  /* '<div><p style="margin:1.5em; "><span style="font-variant:small-caps; font-size:1em; font-weight:bold;">Estreno </span>: '+  datos.release_date +'</p></div>' + */
-                  /* '<div><p style="margin:1.5em; "><span style="font-variant:small-caps; font-size:1em; font-weight:bold;">Resumen </span>: '+  datos.overview +'</p></div>' + */
-                '</section>')            
-                
+                  '<div id="titulo"><h2> '+ datos.title +'</h2><h3>'+ datos.tagline + '</h3><p>'+  datos.overview +'</p><div id="genero-estreno"><p><span> Género </span>: '+  datos.genres[0].name +'</p><p><span> Estreno </span>: '+  datos.release_date +'</p></div></div>'+ 
+                  /* '<div id="genero-estreno"><p><span> Género </span>: '+  datos.genres[0].name +'</p><p><span> Estreno </span>: '+  datos.release_date +'</p></div>'+ */
+                  /* '<div id="titulo"><h2> '+ datos.title +'</h2></div>' +
+                  '<div id="subtitulo"><h3> '+ datos.tagline +'</h3></div>'+
+                  '<div id="resumen"><p><span> Resumen </span>: '+  datos.overview +'</p></div>' + 
+                  '<div id="genero"><p>Género </span>: '+  datos.genres[0].name +'</p></div>' +
+                  '<div id="estreno"><p>Estreno </span>: '+  datos.release_date +'</p></div>' +  */
+                '</section>')       
+                fetch('https://api.themoviedb.org/3/movie/'+ idPelis +'/videos?api_key=da2bc34b7d1c815449e218aadeea9272&language=es')
+                .then(resp => resp.json())
+                .then(resp => {
+                  console.log(resp)
+                  console.log(idPelis)
+                  let rdos = resp.results
+                  console.log(rdos)
+                  console.log(rdos[0].key)
 
-
-         
+                   document.querySelector('.cards-container-peliculas').insertAdjacentHTML ("afterend", 
+                        '<section id="trailer">' +
+                        /* '<iframe src="https://www.youtube.com/embed/' + datos.key +'" frameborder = "0"></iframe>'+  */ 
+                        '<iframe src="https://www.youtube.com/embed/' + rdos[0].key +'" frameborder = "0" width="60%" height="315"></iframe>'+
+                        '</section>')             
+            })
     })
+    
   })
 }
 })          
@@ -70,4 +83,4 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?language=es&page=1&api_key
       
 //Probar: lista x genero https://developer.themoviedb.org/reference/genre-movie-list
 //videos : https://developer.themoviedb.org/reference/movie-videos
-    
+//   
