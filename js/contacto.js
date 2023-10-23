e6b84c31578ac052d3ca026263863dd51ef0699f
CaroGonza <natalia.carolina.gonzalez@gmail.com>
2023-10-23 18:26:24 -0300

    funciones forms y funcionalidad

--- js/contacto.js
+++ js/contacto.js
@@ -1,50 +1,70 @@
 let form = document.querySelector("form")
+form.addEventListener("submit", validar)
+let ok = 0
 
 function validar (e){
-    let ok = 0
+    
+    validarNombre(e)
+    validarTel(e)
+    validarMail(e)
+    validarAcerca(e)
+    validarCheck(e)
+    if (ok == 5){
+        alert("Sus datos han sido enviados. Nos contactaremos a la brevedad")
+    } else{
+        e.preventDefault()
+        ok = 0
+    }
+}
+
+function validarNombre(e){
     if(form.nombre.value == ""){
         alert("Completá tu nombre y apellido")
-        form.nombre.value == ""
-    }else if ((/^[a-zA-Z]{3,20}$/.test(form.nombre.value)) == false){
+    }else if ((/^[a-zA-Z ]{3,40}$/.test(form.nombre.value)) == false){
         alert("Completá tu nombre y apellido, mínimo 3 caractéres") 
     } else{
         ok++
+        return ok
     }
-    if (form.tel.value == ""){
-        alert("Completá tu teléfono")   
-    } else if ((/^\d{10}$/.test(form.tel.value)) == false){
-        alert("Completá un teléfono válido") 
-    }else{
-        ok++
+}
+
+function validarTel (e){ 
+        if (form.tel.value == ""){
+            alert("Completá tu teléfono") 
+            ok = 0  
+        } else if ((/^\d{10}$/.test(form.tel.value)) == false){
+            alert("Completá un teléfono válido") 
+        }else{
+            ok++
+            return ok
+        }
     }
+
+function validarMail(e){
     if (form.email.value == "") { 
         alert("Completa tu E-mail")
     } else if ((/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.email.value)) == false){
         alert("Escribí un e-mail válido") 
     }else{
         ok++
+        return ok
     }
+}
+function validarAcerca(e){
     if (form.acercaDe.value == ""){ 
         alert("Dejanos tus comentarios")
-    } else if ((/^[a-zA-Z0-9]{1,301}$/.test(form.acercaDe.value)) == false){
+    } else if ((/^[a-zA-Z0-9 ]{1,301}$/.test(form.acercaDe.value)) == false){
         alert("Máximo 300 caractéres") 
     }else{
         ok++
+        return ok
     }
+}
+function validarCheck(e){
     if (form.condiciones.checked == false){
         alert("Acepta las condiciones"); 
     }else{
         ok++
-    }
-    if (ok == 5){
-        alert("Sus datos han sido enviados. Nos contactaremos a la brevedad")
-    } else{
-        e.preventDefault()
+        return ok
     }
 }
-
-form.addEventListener("submit", validar)
-
-
-
-
--- js/login.js
+++ js/login.js
@@ -5,8 +5,6 @@ let btnLogin = document.querySelector("#link-login")
 let login = document.querySelector("#login")
 let registro = document.querySelector("#registro")
 
-
-
 //Alterna form login con form registro
 btnLogin.addEventListener("click", function(){
     registro.style.display = "none"
@@ -22,15 +20,51 @@ btnRegistro.addEventListener("click", function(){
 formRegistro.addEventListener("submit", validarRegistro)
 formLogin.addEventListener("submit", validarLogin)
 
+//Validación forms
+let ok = 0
 function validarLogin (e){
-    let ok = 0
+    validarMailLogin(e)
+    validarPassLogin(e)
+    if (ok == 2){
+        alert("Sus datos son correctos")
+        e.preventDefault()
+        formLogin.reset()
+    }else{
+        e.preventDefault()
+        ok = 0
+    }
+}
+
+function validarRegistro (e){
+    validarNombre(e)
+    validarApellido(e)
+    validarTel(e)
+    validarMailRegistro(e)
+    validarPassRegistro(e)
+    validarCheck(e)
+    if (ok == 6){
+        alert("Se ha registrado con éxito.")
+        e.preventDefault()
+        formRegistro.reset()
+    } else{
+        e.preventDefault()
+        ok = 0
+    }
+}
+
+//Funciones individuales de validacion. Como hacer p usar la misma funcion p validar mail y pass, si tengo q apuntar a 2 form distintos???
+
+function validarMailLogin(e){
     if (formLogin.email.value == "") { 
         alert("Completa tu E-mail")
     } else if ((/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formLogin.email.value)) == false){
         alert("Escribí un e-mail válido") 
     }else{
         ok++
+        return ok
     }
+}
+function validarPassLogin(e){
     if (formLogin.password.value == ""){ 
         alert("Ingresa una clave")
     } else if ((/\w{8,10}/.test(formLogin.password.value)) == false){
@@ -38,64 +72,65 @@ function validarLogin (e){
         console.log(formLogin.password.value) 
     }else{
         ok++
-    }
-    if (ok == 2){
-        alert("Sus datos son correctos")
-    }else{
-        e.preventDefault()
+        return ok
     }
 }
-
-
-function validarRegistro (e){
-    let ok = 0
-    if(formRegistro.nombre.value == ""){
-        alert("Completá tu nombre y apellido")
-        formRegistro.nombre.value == ""
-        //e.preventDefault()
-    }else if ((/^[a-zA-Z]{3,20}$/.test(formRegistro.nombre.value)) == false){
-        alert("Completá tu nombre y apellido, mínimo 3 caractéres")
-        //e.preventDefault() 
-    } else{
-        ok++
-    }
-    if (formRegistro.tel.value == ""){
-        alert("Completá tu teléfono") 
-        //e.preventDefault()  
-    } else if ((/^\d{10}$/.test(formRegistro.tel.value)) == false){
-        alert("Completá un teléfono válido")
-        //e.preventDefault() 
-    }else{
-        ok++
-    }
-    if (formRegistro.email.value == "") { 
-        alert("Completa tu E-mail")
-        //e.preventDefault()
-    } else if ((/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formRegistro.email.value)) == false){
-        alert("Escribí un e-mail válido")
-        //e.preventDefault() 
-    }else{
-        ok++
-    }
+function validarPassRegistro(e){
     if (formRegistro.password.value == ""){ 
         alert("Ingresa una clave")
     } else if ((/\w{8,10}/.test(formRegistro.password.value)) == false){
         alert("Contraseña incorrecta")
+        console.log(formRegistro.password.value) 
     }else{
         ok++
-    }
-    if (formRegistro.condiciones.checked == false){
-        alert("Acepta las condiciones");
-        //e.preventDefault() 
-    }else{
-        ok++
-    }
-    if (ok == 5){
-        alert("Se ha registrado con éxito.")
-    } else{
-        e.preventDefault()
+        return ok
     }
 }
-
-
-
+function validarMailRegistro(e){
+    if (formRegistro.email.value == "") { 
+        alert("Completa tu E-mail")
+    } else if ((/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formRegistro.email.value)) == false){
+        alert("Escribí un e-mail válido") 
+    }else{
+        ok++
+        return ok
+    }
+}
+function validarNombre(e){
+    if(formRegistro.nombre.value == ""){
+        alert("Completá tu nombre")
+    }else if ((/^[a-zA-Z ]{3,20}$/.test(formRegistro.nombre.value)) == false){
+        alert("Completá tu nombre, mínimo 3 caractéres") 
+    } else{
+        ok++
+        return ok
+    }
+}
+function validarApellido(e){
+    if(formRegistro.apellido.value == ""){
+        alert("Completá tu apellido")
+    }else if ((/^[a-zA-Z ]{3,20}$/.test(formRegistro.apellido.value)) == false){
+        alert("Completá tu apellido mínimo 3 caractéres")
+    } else{
+        ok++
+        return ok
+    }
+}
+function validarTel (e){ 
+    if (formRegistro.tel.value == ""){
+        alert("Completá tu teléfono")  
+    } else if ((/^\d{10}$/.test(formRegistro.tel.value)) == false){
+        alert("Completá un teléfono válido") 
+    }else{
+        ok++
+        return ok
+    }
+}
+function validarCheck(e){
+    if (formRegistro.condiciones.checked == false){
+        alert("Acepta las condiciones"); 
+    }else{
+        ok++
+        return ok
+    }
+}
